a5bee70f9a8cb9891d6ea21dc438cc6d515deae7
putty182 <putty182@gmail.com>
2020-09-28 23:47:40 +1000

    Delete version-at-docker-hub.sh

--- version-at-docker-hub.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eou pipefail
-
-version=$1
-url="https://hub.docker.com/v2/repositories/sebastianalbers/spotify-ripper/tags/${version}"
-curl -s "$url" | jq '.name' | grep "${version}"
-
