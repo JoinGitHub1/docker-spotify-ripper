c85c8dc027206aba38a63f1b18111c6fc62669f9
JoinGitHub1 <user@user>
2021-01-28 09:40:00 +0100

    Simplify entrypoint

--- start.sh
+++ start.sh
@@ -2,18 +2,5 @@
 
 set -u
 
-USER_ID="${USER_ID:-1000}"
-GROUP_ID="${GROUP_ID:-1000}"
-
-USER=spot
-GROUP=spot
-
-# add user and group
-addgroup --gid "${GROUP_ID}" "${GROUP}" > /dev/null
-adduser --system --shell /bin/false --gid "${GROUP_ID}" --uid "${USER_ID}" "${USER}" > /dev/null
-ln -s /config "/home/${USER}/.spotify-ripper"
-
-chown -R "${USER}:${GROUP}" /music
-
-su "${USER}" -s /bin/sh -c 'PYTHONIOENCODING=UTF-8 /usr/local/bin/spotify-ripper "${0}" "$@"' -- "$@"
-
+ln -s /config /root/.spotify-ripper
+/bin/sh -c 'PYTHONIOENCODING=UTF-8 /usr/local/bin/spotify-ripper "${0}" "$@"' -- "$@"
